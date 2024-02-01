8f63de3eda49235dc668f3977cb46549350c4fed
Jacob Jensen <jakobeistheone@gmail.com>
2024-02-01 09:52:55 -0600

    Moved things around

---
+++ managers/__init__.py
@@ -0,0 +1,3 @@
+from .chatgpt_manager import ChatGPTManager
+from .speech_recognition_manager import SpeechRecognitionManager
+from .tts_manager import TTSManager
--- managers/speech_recognition_manager.py
+++ managers/speech_recognition_manager.py
@@ -1,13 +1,12 @@
-from speech_recognition import Recognizer, Microphone, AudioFile, UnknownValueError
+from speech_recognition import AudioFile, Microphone, Recognizer, UnknownValueError
 
 
 class SpeechRecognitionManager:
-    
-    def __init__(self, whisper_model: str = "small"):
+    def __init__(self, whisper_model: str = "base"):
         self.recognizer = Recognizer()
         self.microphone = Microphone()
         self.model = whisper_model
-    
+
     def get_microphone_input(self):
         try:
             with self.microphone as source:
@@ -17,8 +16,8 @@ class SpeechRecognitionManager:
         except UnknownValueError:
             print("Unable to recognize microphone audio")
             return None
-        
-    def audio_to_text(self, audio_data = None, audio_file: str | None = None) -> str:
+
+    def audio_to_text(self, audio_data=None, audio_file: str | None = None) -> str:
         if audio_file is not None:
             with AudioFile(audio_file) as source:
                 self.recognizer.adjust_for_ambient_noise(source, duration=0.5)
@@ -26,18 +25,17 @@ class SpeechRecognitionManager:
         elif audio_data is None:
             exit("audio_data and audio_file are not set")
         return self.recognizer.recognize_whisper(audio_data, model=self.model)
-            
+
     def speech_to_text(self) -> str | None:
         audio_data = self.get_microphone_input()
         if audio_data is None:
             return None
         return self.audio_to_text(audio_data)
-    
-    
+
+
 if __name__ == "__main__":
-    speech_recognize_manager = SpeechRecognitionManager(whisper_model="small")
+    speech_recognize_manager = SpeechRecognitionManager(whisper_model="base")
     # text = speech_recognize_manager.speech_to_text()
     text = speech_recognize_manager.audio_to_text(audio_file="sample.wav")
     print(text)
     quit()
-    
